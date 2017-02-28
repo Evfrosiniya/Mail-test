df0538210899c42531a4906ec7de66ea5e997bd1
Evfrosiniya <evfrosiniya.z@gmail.ru>
2017-02-28 19:40:10 +0300

    add footer, logo

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const compression = require('compression');
 const app = express();
 app.use(compression());
 app.use('/', express.static('dist'));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 7000, () => {
-	console.log(`App started on port ${process.env.PORT || 7000}`);
+app.listen(process.env.PORT || 8000, () => {
+	console.log(`App started on port ${process.env.PORT || 8000}`);
 });
