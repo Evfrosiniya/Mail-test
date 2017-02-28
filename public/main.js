cdfdc57c9f324de7bf1fc6d31955d9b2f7b3f8f3
Evfrosiniya <evfrosiniya.z@gmail.ru>
2017-02-28 22:02:44 +0300

    add tabs

--- public/main.js
+++ public/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 import './css/main.scss';
+
+
+let tabChange = function (evt, name, tabname) {
+	console.log(this);
+	let  i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName('content__vacancy-requirement-list');
+	for (i = 0; i < 2; i++) {
+		tabcontent[i].classList.remove('active-list');
+	}
+
+	tablinks = document.getElementsByClassName('content__vacancy-requirement_nav-tab');
+	for (i = 0; i < 2; i++) {
+		tablinks[i].classList.remove('active-tab');
+	}
+	document.getElementById(tabname).classList.add('active-tab');
+	document.getElementById(name).classList.add('active-list');
+}
