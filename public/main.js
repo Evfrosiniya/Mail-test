350da0af71b43f83539af534308c3e127e85e247
Evfrosiniya <evfrosiniya.z@gmail.ru>
2017-03-01 23:50:05 +0300

    change webpack

--- public/main.js
+++ public/main.js
@@ -3,27 +3,43 @@
 import './css/main.scss';
 
 
-let tabChange = function (evt, name, tabname) {
-	let  i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName('content__vacancy-requirement-list');
-	for (i = 0; i < 2; i++) {
+window.tabChange = function (evt, name, tabname) {
+
+	let tabcontent = document.getElementsByClassName('content__vacancy-requirement-list');
+	for (let i = 0; i < 2; i++) {
 		tabcontent[i].classList.remove('active-list');
 	}
 
-	tablinks = document.getElementsByClassName('content__vacancy-requirement_nav-tab');
-	for (i = 0; i < 2; i++) {
+	let tablinks = document.getElementsByClassName('content__vacancy-requirement_nav-tab');
+	for (let i = 0; i < 2; i++) {
 		tablinks[i].classList.remove('active-tab');
 	}
 	document.getElementById(tabname).classList.add('active-tab');
 	document.getElementById(name).classList.add('active-list');
 }
-let photoChange = function (evt, id) {
+window.photoChange = function (evt, id) {
 
-	let  i, photocontent;
-	photocontent = document.getElementsByClassName('photos_elem');
-	for (i = 0; i < photocontent.length; i++) {
+	let photocontent = document.getElementsByClassName('photos_elem');
+	for (let i = 0; i < photocontent.length; i++) {
 		photocontent[i].classList.remove('active');
 	}
 
 	document.getElementById(id).classList.add('active');
 }
+
+//let button1 = ...;
+//let button2 = ...;
+
+//button1.addEventListener('click', tabChange);
+//button2.addEventListener('click', photoChange);
+
+let tabList=document.getElementsByClassName('content__vacancy-requirement_nav-tab');
+for (let i = 0; i < tabList.length; i++) {
+	tabList.addEventListner('click', function (event) {
+		const click = event.target;
+		const uniqParam = click.getAttribute('data-uniq-param');
+		console.log(uniqParam);
+		tabChange()
+	});
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const precss = require('precss');
 const autoprefixer = require('autoprefixer');
 const HtmlPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	devtool: 'eval-source-map',
@@ -30,8 +31,11 @@ module.exports = {
 				}
 			},
 			{
-				test: /\.(s)?css/,
-				loader: 'style-loader!css-loader!postcss-loader!sass-loader'
+				test: /\.s?css$/,
+				loader: ExtractTextPlugin.extract({
+					fallbackLoader: 'style-loader',
+					loader: ['css-loader?minimize', 'postcss-loader', 'sass-loader']
+				})
 			},
 			{
 				test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/,
@@ -55,6 +59,7 @@ module.exports = {
 		new HtmlPlugin({
 			filename: 'index.html',
 			template: path.resolve(__dirname, 'public/index.html')
-		})
+		}),
+		new ExtractTextPlugin(path.join('css', '[name].bundle.[hash].css'))
 	]
 };
