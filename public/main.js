b76a7ef713803011b97b2da64bae4c0cdbdc6ad6
Evfrosiniya <evfrosiniya.z@gmail.ru>
2017-03-12 22:28:29 +0300

    final changes

--- public/main.js
+++ public/main.js
@@ -3,53 +3,51 @@
 import './css/main.scss';
 
 
-let tabChange = function (id, listid) {
+const tabChange = function (id, listid) {
 
-	let tabcontent = document.getElementsByClassName('content__vacancy-requirement-list');
+	const tabcontent = document.getElementsByClassName('content__vacancy-requirement-list');
 	for (let i = 0; i < 2; i++) {
 		tabcontent[i].classList.remove('active-list');
 	}
 
-	let tablinks = document.getElementsByClassName('content__vacancy-requirement_nav-tab');
+	const tablinks = document.getElementsByClassName('content__vacancy-requirement_nav-tab');
 	for (let i = 0; i < 2; i++) {
 		tablinks[i].classList.remove('active-tab');
 	}
 	document.getElementById(id).classList.add('active-tab');
 	document.getElementById(listid).classList.add('active-list');
-}
+};
 
 
-let tabList = document.getElementsByClassName('content__vacancy-requirement_nav-tab');
+const tabList = document.getElementsByClassName('content__vacancy-requirement_nav-tab');
 for (let i = 0; i < tabList.length; i++) {
 	tabList[i].addEventListener('click', function (event) {
 		const click = event.target;
 		const uniqParam = click.getAttribute('data-uniq-param');
-		tabChange(uniqParam, uniqParam + '-list')
+		tabChange(uniqParam, uniqParam + '-list');
 	});
 }
 
 
-let width = document.getElementById('photos').offsetWidth;
+const width = document.getElementById('photos').offsetWidth;
 
-let lis = document.getElementsByClassName('photos_elem');
+const lis = document.getElementsByClassName('photos_elem');
 for (let i = 0; i < lis.length; i++) {
 	lis[i].style.position = 'relative';
 	lis[i].style.width = width + 'px';
-	let span = document.createElement('span');
+	const span = document.createElement('span');
 	span.style.cssText = 'position:absolute;left:0;top:0';
 	span.innerHTML = i + 1;
 	lis[i].appendChild(span);
-
-
 }
 
-let miniLis = document.getElementsByClassName('photos-mini_elem');
+const miniLis = document.getElementsByClassName('photos-mini_elem');
 for (let i = 0; i < miniLis.length; i++) {
 	miniLis[i].id = i;
 }
 
 let position = 0;
-let photoList = document.getElementsByClassName('photos-mini_elem');
+const photoList = document.getElementsByClassName('photos-mini_elem');
 for (let i = 0; i < photoList.length; i++) {
 	photoList[i].addEventListener('click', function (event) {
 		for (let j = 0; j < photoList.length; j++) {
@@ -57,68 +55,68 @@ for (let i = 0; i < photoList.length; i++) {
 		}
 		photoList[i].classList.add('active-mini');
 
-		let count = photoList[i].id;
+		const count = photoList[i].id;
 		position = width * Number(count);
 		document.getElementById('photos-ul').style.marginLeft = -position + 'px';
 	});
 }
 
 
-let buttonSend = document.getElementById('sendForm');
+const buttonSend = document.getElementById('sendForm');
 buttonSend.addEventListener('click', function (event) {
 	event.preventDefault();
 	document.getElementById('error-text').removeChild(document.getElementById('error'));
-	let errorMessage = document.createElement('span');
+	const errorMessage = document.createElement('span');
 	errorMessage.setAttribute('id', 'error');
 
 	const formdata = {
-		name: document.getElementsByTagName("input")[0].value,
-		surname: document.getElementsByTagName("input")[1].value,
-		email: document.getElementsByTagName("input")[2].value,
-		tel: document.getElementsByTagName("input")[3].value,
+		name: document.getElementsByTagName('input')[0].value,
+		surname: document.getElementsByTagName('input')[1].value,
+		email: document.getElementsByTagName('input')[2].value,
+		tel: document.getElementsByTagName('input')[3].value,
 	};
 
 	if (!formdata.name) {
-		document.getElementsByTagName("input")[0].classList.add('empty');
+		document.getElementsByTagName('input')[0].classList.add('empty');
 
 		errorMessage.innerHTML = 'Заполните поля правильно!';
 	} else {
-		document.getElementsByTagName("input")[0].classList.remove('empty');
+		document.getElementsByTagName('input')[0].classList.remove('empty');
 	}
 	if (!formdata.surname) {
-		document.getElementsByTagName("input")[1].classList.add('empty');
+		document.getElementsByTagName('input')[1].classList.add('empty');
 		errorMessage.innerHTML = 'Заполните поля правильно!';
 	} else {
-		document.getElementsByTagName("input")[1].classList.remove('empty');
+		document.getElementsByTagName('input')[1].classList.remove('empty');
 	}
 	if (!formdata.email) {
-		document.getElementsByTagName("input")[2].classList.add('empty');
+		document.getElementsByTagName('input')[2].classList.add('empty');
 		errorMessage.innerHTML = 'Заполните поля правильно!';
 	} else {
-		document.getElementsByTagName("input")[2].classList.remove('empty');
+		document.getElementsByTagName('input')[2].classList.remove('empty');
 	}
 	if (!formdata.tel) {
-		document.getElementsByTagName("input")[3].classList.add('empty');
+		document.getElementsByTagName('input')[3].classList.add('empty');
 		errorMessage.innerHTML = 'Заполните поля правильно!';
 	} else {
-		document.getElementsByTagName("input")[3].classList.remove('empty');
+		document.getElementsByTagName('input')[3].classList.remove('empty');
 	}
 
-	let at = formdata.email.indexOf("@");
-	let dot = formdata.email.indexOf(".");
+	const at = formdata.email.indexOf('@');
+	const dot = formdata.email.indexOf('.');
 	if (at < 1 || dot < 1) {
-		document.getElementsByTagName("input")[2].classList.add('empty');
+		document.getElementsByTagName('input')[2].classList.add('empty');
 		errorMessage.innerHTML = 'Заполните поля правильно!';
 	} else {
 		document.getElementsByTagName('input')[2].classList.remove('empty');
 	}
 
-	let regexp = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+	const regexp = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
 	if (formdata.tel.search(regexp) == -1) {
 		document.getElementsByTagName('input')[3].classList.add('empty');
 		errorMessage.innerHTML = 'Заполните поля правильно!';
 	} else {
-		document.getElementsByTagName("input")[3].classList.remove('empty');
+		document.getElementsByTagName('input')[3].classList.remove('empty');
 	}
 	if (errorMessage.innerHTML === '') {
 		if (screen.width > 1050) {
@@ -130,16 +128,18 @@ buttonSend.addEventListener('click', function (event) {
 			document.getElementsByClassName('wrapper')[0].style.background = 'transparent';
 			document.getElementsByClassName('header__vacancy')[0].style.color = '#ffffff';
 
-			let img = document.createElement('IMG');
+			const img = document.createElement('IMG');
 			img.src = '/public/images/ok.svg';
-			let span1 = document.createElement('span');
-			let span2 = document.createElement('span');
+			const span1 = document.createElement('span');
+			const span2 = document.createElement('span');
 			span1.innerHTML = 'Получилось!';
 			span2.innerHTML = 'Мы с вами скоро свяжемся!';
 
-			let footer = document.createElement('span');
+			const footer = document.createElement('span');
 			footer.classList.add('footer-mobile');
 			footer.innerHTML = '© Mail.ru HR TEAM';
+
+
 			document.getElementById('vacancy-form-done-mobile').appendChild(img);
 			document.getElementById('vacancy-form-done-mobile').appendChild(span1);
 			document.getElementById('vacancy-form-done-mobile').appendChild(span2);
--- server.js
+++ server.js
@@ -3,11 +3,12 @@
 const express = require('express');
 const parser = require('body-parser');
 const compression = require('compression');
+const path = require('path');
 
 const app = express();
 app.use(compression());
 app.use('/', express.static('dist'));
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.listen(process.env.PORT || 8000, () => {
 	console.log(`App started on port ${process.env.PORT || 8000}`);
